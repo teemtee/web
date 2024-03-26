5efede7fe0009bb6d4f6e1ed31b182779618f5de
Tom Koscielniak <tkosciel@redhat.com>
2024-03-26 12:55:49 +0100

    Use f-string in html generator

--- src/html_generator.py
+++ src/html_generator.py
@@ -12,17 +12,18 @@ def generate_test_html_page(test: Test, logger: Logger) -> str:
     logger.print("Generating the HTML file...")
     full_url = test.web_link()
     # Adding the input data to the HTML file
-    file_html = ('''<html>
+    file_html = (f'''<html>
     <head>
     <title>HTML File</title>
     <meta charset="UTF-8">
     </head> 
     <body>
-    name: ''' + str(test.name) + '''<br>
-    summary: ''' + str(test.summary) + '''<br>
-    url: <a href=\"''' + full_url + "\" target=\"_blank\">" + full_url + '''</a><br>
-    ref: ''' + str(test.fmf_id.ref) + '''<br>
-    contact: ''' + str(test.contact) + '''<br>
+    name: {test.name}<br>
+    summary: {test.summary}<br>
+    description: {test.description}<br>
+    url: <a href=\"{full_url}\" target=\"_blank\">{full_url}</a><br>
+    ref: {test.fmf_id.ref}<br>
+    contact: {test.contact}<br>
     </body>
     </html>''')
     logger.print("HTML file generated successfully!", color="green")
@@ -39,16 +40,17 @@ def generate_plan_html_page(plan: Plan, logger: Logger) -> str:
     logger.print("Generating the HTML file...")
     full_url = plan.web_link()
     # Adding the input data to the HTML file
-    file_html = ('''<html>
+    file_html = (f'''<html>
     <head>
     <title>HTML File</title>
     <meta charset="UTF-8">
     </head> 
     <body>
-    name: ''' + str(plan.name) + '''<br>
-    summary: ''' + str(plan.summary) + '''<br>
-    url: <a href=\"''' + full_url + "\" target=\"_blank\">" + full_url + '''</a><br>
-    ref: ''' + str(plan.fmf_id.ref) + '''<br>
+    name: {plan.name}<br>
+    summary: {plan.summary}<br>
+    description: {plan.description}<br>
+    url: <a href=\"{full_url}\" target=\"_blank\">{full_url}</a><br>
+    ref: {plan.fmf_id.ref}<br>
     </body>
     </html>''')
     logger.print("HTML file generated successfully!", color="green")
@@ -67,22 +69,24 @@ def generate_testplan_html_page(test: tmt.Test, plan: tmt.Plan, logger: Logger) 
     full_url_test = test.web_link()
     full_url_plan = plan.web_link()
     # Adding the input data to the HTML file
-    file_html = ('''<html>
+    file_html = (f'''<html>
     <head>
     <title>HTML File</title>
     <meta charset="UTF-8">
     </head> 
     <body>
-    name: ''' + str(test.name) + '''<br>
-    summary: ''' + str(test.summary) + '''<br>
-    url: <a href=\"''' + full_url_test + "\" target=\"_blank\">" + full_url_test + '''</a><br>
-    ref: ''' + str(test.fmf_id.ref) + '''<br>
-    contact: ''' + str(test.contact) + '''<br>
+    name: {test.name}<br>
+    summary: {test.summary}<br>
+    description: {test.description}<br>
+    url: <a href=\"{full_url_test}\" target=\"_blank\">{full_url_test}</a><br>
+    ref: {test.fmf_id.ref}<br>
+    contact: {test.contact}<br>
     <br>
-    name: ''' + str(plan.name) + '''<br>
-    summary: ''' + str(plan.summary) + '''<br>
-    url: <a href=\"''' + full_url_plan + "\" target=\"_blank\">" + full_url_plan + '''</a><br>
-    ref: ''' + str(plan.fmf_id.ref) + '''<br>
+    name: {plan.name}<br>
+    summary: {plan.summary}<br>
+    description: {plan.description}<br>
+    url: <a href=\"{full_url_plan}\" target=\"_blank\">{full_url_plan}</a><br>
+    ref: {plan.fmf_id.ref}<br>
     </body>
     </html>''')
     logger.print("HTML file generated successfully!", color="green")
