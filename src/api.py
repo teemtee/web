159c33580faa13f16485ddb01ff94a9a0686ddd6
Tom Koscielniak <tkosciel@redhat.com>
2024-04-23 13:11:48 +0200

    Add hostname env for status callback

--- src/api.py
+++ src/api.py
@@ -32,10 +32,14 @@ def find_test(
         plan_ref: str = Query("default", alias="plan-ref"),
         out_format: str = Query("json", alias="format")
 ):
+    # Parameter validations
     if (test_url is None and test_name is not None) or (test_url is not None and test_name is None):
         return "Invalid arguments!"
     if (plan_url is None and plan_name is not None) or (plan_url is not None and plan_name is None):
         return "Invalid arguments!"
+    if plan_url is None and plan_name is None and test_url is None and test_name is None:
+        return "Missing arguments!"
+    # Disable Celery if not needed
     if os.environ.get("USE_CELERY") == "false":
         html_page = service.main(test_url, test_name, test_ref, plan_url, plan_name, plan_ref, out_format)
         return html_page
@@ -44,7 +48,7 @@ def find_test(
     if out_format == "html":
         global format_html
         format_html = True
-        status_callback_url = f'/status?task-id={r.task_id}&html=true'
+        status_callback_url = f'{os.getenv("API_HOSTNAME")}/status?task-id={r.task_id}&html=true'
         return HTMLResponse(content=html_generator.generate_status_callback(r, status_callback_url))
     else:
         format_html = False  # To set it back to False after a html format request
@@ -56,7 +60,7 @@ def status(task_id: str = Query(None, alias="task-id"),
            html: str = Query("false")) -> TaskOut | HTMLResponse:
     r = service.main.app.AsyncResult(task_id)
     if html == "true":
-        status_callback_url = f'/status?task-id={r.task_id}&html=true'
+        status_callback_url = f'{os.getenv("API_HOSTNAME")}/status?task-id={r.task_id}&html=true'
         return HTMLResponse(content=html_generator.generate_status_callback(r, status_callback_url))
     return _to_task_out(r)
 
@@ -66,5 +70,5 @@ def _to_task_out(r: AsyncResult) -> TaskOut | str:
         id=r.task_id,
         status=r.status,
         result=r.traceback if r.failed() else r.result,
-        status_callback_url=f'{os.getenv("HOSTNAME")}/status?task-id={r.task_id}'
+        status_callback_url=f'{os.getenv("API_HOSTNAME")}/status?task-id={r.task_id}'
     )
