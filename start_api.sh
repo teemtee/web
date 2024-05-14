934d4bdf7b114bd40d086d6f2ae12db2d11db68a
Tom Koscielniak <tkosciel@redhat.com>
2024-05-14 20:52:34 +0200

    Fix hanging actions and remove start script

--- start_api.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# For local dev purposes
-docker run --rm --name some-redis -p 6379:6379 redis:latest &
-celery --app=src.api.service worker --concurrency=1 --loglevel=INFO &
-uvicorn src.api:app --reload && fg
