cf99aaff34a87bd37305ed1093dae7f4e0c97d52
Martin Hoyer <mhoyer@redhat.com>
2024-08-20 13:44:35 +0200

    Use Jinja in html_generator

--- tests/unit/test_html_generator.py
+++ tests/unit/test_html_generator.py
@@ -9,5 +9,5 @@ class TestHtmlGenerator:
     def test_generate_test_html(self):
         logger = tmt.Logger(logging.getLogger("tmt-logger"))
         test = tmt.Tree(logger=logger).tests(names=["/tests/objects/sample_test"])[0]
-        data = html_generator.generate_test_html_page(test, logger)
-        assert 'name: /tests/objects/sample_test<br>' in data
+        data = html_generator.generate_html_page(test, logger)
+        assert 'name: /tests/objects/sample_test</p>' in data
