cf99bbc9c45c322dfbee0e264a4b48cf21974bba
Martin Hoyer <mhoyer@redhat.com>
2024-09-03 12:53:23 +0200

    Re-use existing dict generation for yaml

--- src/tmt_web/generators/json_generator.py
+++ src/tmt_web/generators/json_generator.py
@@ -9,7 +9,6 @@ def _create_json_data(obj: Test | Plan, logger: tmt.Logger) -> dict[str, Any]:
     """
     Helper function to create the JSON data from a test or plan object
     """
-    logger.print("Generating the JSON file...")
     full_url = obj.web_link()
     return {
         "name": obj.name,
@@ -38,6 +37,7 @@ def generate_test_json(test: tmt.Test, logger: tmt.Logger) -> str:
     :return:
     """
     data = _create_json_data(test, logger)
+    logger.print("Generating the JSON file...")
     return json.dumps(data)
 
 
@@ -49,6 +49,7 @@ def generate_plan_json(plan: tmt.Plan, logger: tmt.Logger) -> str:
     :return:
     """
     data = _create_json_data(plan, logger)
+    logger.print("Generating the JSON file...")
     return json.dumps(data)
 
 
@@ -65,4 +66,5 @@ def generate_testplan_json(test: tmt.Test, plan: tmt.Plan, logger: tmt.Logger) -
         "test": _create_json_data(test, logger),
         "plan": _create_json_data(plan, logger),
     }
+    logger.print("Generating the JSON file...")
     return json.dumps(data)
