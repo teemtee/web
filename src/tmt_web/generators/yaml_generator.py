0d444490f3d9cdf2713a82f665c39a9bba475074
Martin Hoyer <mhoyer@redhat.com>
2024-09-03 12:53:23 +0200

    Re-use existing dict generation for yaml

--- src/tmt_web/generators/yaml_generator.py
+++ src/tmt_web/generators/yaml_generator.py
@@ -1,6 +1,12 @@
 import tmt
 from tmt import Logger
 
+from tmt_web.generators.json_generator import _create_json_data
+
+
+def print_success(logger) -> None:
+    logger.print("YAML file generated successfully!", color="green")
+
 
 def generate_test_yaml(test: tmt.Test, logger: Logger) -> str:
     """
@@ -9,27 +15,8 @@ def generate_test_yaml(test: tmt.Test, logger: Logger) -> str:
     :param logger: tmt.Logger instance
     :return:
     """
-    logger.print("Generating the YAML file...")
-    full_url = test.web_link()
-    data = {
-        "name": test.name,
-        "summary": test.summary,
-        "description": test.description,
-        "url": full_url,
-        "ref": test.fmf_id.ref,
-        "contact": test.contact,
-        "tag": test.tag,
-        "tier": test.tier,
-        "id": test.id,
-        "fmf-id": {
-            "url": test.fmf_id.url,
-            "path": test.fmf_id.path.as_posix() if test.fmf_id.path is not None else None,
-            "name": test.fmf_id.name,
-            "ref": test.fmf_id.ref,
-        }
-    }
-    yaml_data = tmt.utils.dict_to_yaml(data)
-    logger.print("YAML file generated successfully!", color="green")
+    yaml_data = tmt.utils.dict_to_yaml(_create_json_data(test, logger))
+    print_success(logger)
     return yaml_data
 
 
@@ -40,27 +27,8 @@ def generate_plan_yaml(plan: tmt.Plan, logger: Logger) -> str:
     :param logger: tmt.Logger instance
     :return:
     """
-    logger.print("Generating the YAML file...")
-    full_url = plan.web_link()
-    data = {
-        "name": plan.name,
-        "summary": plan.summary,
-        "description": plan.description,
-        "url": full_url,
-        "ref": plan.fmf_id.ref,
-        "contact": plan.contact,
-        "tag": plan.tag,
-        "tier": plan.tier,
-        "id": plan.id,
-        "fmf-id": {
-            "url": plan.fmf_id.url,
-            "path": plan.fmf_id.path.as_posix() if plan.fmf_id.path is not None else None,
-            "name": plan.fmf_id.name,
-            "ref": plan.fmf_id.ref,
-        }
-    }
-    yaml_data = tmt.utils.dict_to_yaml(data)
-    logger.print("YAML file generated successfully!", color="green")
+    yaml_data = tmt.utils.dict_to_yaml(_create_json_data(plan, logger))
+    print_success(logger)
     return yaml_data
 
 
@@ -72,45 +40,10 @@ def generate_testplan_yaml(test: tmt.Test, plan: tmt.Plan, logger: Logger) -> st
     :param logger: tmt.Logger instance
     :return:
     """
-    logger.print("Generating the YAML file...")
-    full_url_test = test.web_link()
-    full_url_plan = plan.web_link()
     data = {
-        "test": {
-            "name": test.name,
-            "summary": test.summary,
-            "description": test.description,
-            "url": full_url_test,
-            "ref": test.fmf_id.ref,
-            "contact": test.contact,
-            "tag": test.tag,
-            "tier": test.tier,
-            "id": test.id,
-            "fmf-id": {
-                "url": test.fmf_id.url,
-                "path": test.fmf_id.path.as_posix() if test.fmf_id.path is not None else None,
-                "name": test.fmf_id.name,
-                "ref": test.fmf_id.ref,
-            }
-        },
-        "plan": {
-            "name": plan.name,
-            "summary": plan.summary,
-            "description": plan.description,
-            "url": full_url_plan,
-            "ref": plan.fmf_id.ref,
-            "contact": plan.contact,
-            "tag": plan.tag,
-            "tier": plan.tier,
-            "id": plan.id,
-            "fmf-id": {
-                "url": plan.fmf_id.url,
-                "path": plan.fmf_id.path.as_posix() if plan.fmf_id.path is not None else None,
-                "name": plan.fmf_id.name,
-                "ref": plan.fmf_id.ref,
-            }
-        }
+        "test": _create_json_data(test, logger),
+        "plan": _create_json_data(plan, logger),
     }
     yaml_data = tmt.utils.dict_to_yaml(data)
-    logger.print("YAML file generated successfully!", color="green")
+    print_success(logger)
     return yaml_data
