54ee366be8b0198670e384cf876f73dba7f804df
Martin Hoyer <mhoyer@redhat.com>
2024-10-30 18:01:44 +0100

    Polish github actions, Containerfile, compose

--- Containerfile
+++ Containerfile
@@ -11,4 +11,8 @@ RUN SETUPTOOLS_SCM_PRETEND_VERSION=0.1.0.dev0 pip install .
 COPY /entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
+LABEL org.opencontainers.image.title="tmt-web"
+LABEL org.opencontainers.image.description="Web API for checking tmt tests, plans and stories"
+LABEL org.opencontainers.image.source="https://github.com/teemtee/tmt-web"
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
+error() {
+    echo "ERROR: $1" >&2
+    exit 1
+}
+
+# Handle signals
+trap 'kill -TERM $PID' TERM INT
+
 # Name of container to start
 APP=$1
 
-[ -z "$APP" ] && { error "No api to run passed to entrypoint script"; exit 1; }
+[ -z "$APP" ] && error "No app to run passed to entrypoint script"
 
 case $APP in
     uvicorn)
         COMMAND="uvicorn tmt_web.api:app --reload --host 0.0.0.0 --port 8000"
         ;;
     celery)
-        COMMAND="celery --app=tmt_web.api.service worker --loglevel=INFO"
+        COMMAND="celery --app=tmt_web.service worker --loglevel=INFO"
         ;;
     *)
-        echo "Unknown app '$APP'"
-        exit 1
+        error "Unknown app '$APP'"
         ;;
 esac
 
